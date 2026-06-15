b694af60c75d4b445c1f4e4e5c8a276cf70f09b4
Grace Wang <92639381+GraceW18@users.noreply.github.com>
2026-06-15 19:14:11 +0800

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, k - 1);
+    }
+    private int quickSelect(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (true) {
+            if (right <= left + 1) {
+                if (right == left && nums[right] > nums[left]) {
+                    swap(nums, left, right);
+                }
+                return nums[k];
+            }
+            int pivot = split(nums, left, right);
+            if (pivot >= k) right = pivot - 1;
+            if (pivot <= k) left = pivot + 1;
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private int split(int[] nums, int left, int right) {
+        int mid = (left + right) / 2;
+        swap(nums, mid, left + 1);
+        if (nums[left] < nums[right]) {
+            swap(nums, left, right);
+        }
+        if (nums[left + 1] < nums[right]) {
+            swap(nums, left + 1, right);
+        }
+        if (nums[left] < nums[left + 1]) {
+            swap(nums, left, left + 1);
+        }
+        int pivot = nums[left + 1];
+        int i = left + 1;
+        int j = right;
+        while (true) {
+            while (nums[++i] > pivot);
+            while (nums[--j] < pivot);
+            if (i > j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        nums[left + 1] = nums[j];
+        nums[j] = pivot;
+        return j;
+    }
+}
